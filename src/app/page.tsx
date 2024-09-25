274f153ff84ca458735a6bb44878adc7b6368ea6
Raj Dwivedi <rajdwivedipc@gmail.com>
2024-09-25 12:15:24 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+   <section id={"welcome"}>
+       <div className={'flex w-full h-screen items-center justify-center'}>
+           <h1>Welcome to Suraksha Kawach</h1>
+       </div>
+   </section>
+  );
+}
