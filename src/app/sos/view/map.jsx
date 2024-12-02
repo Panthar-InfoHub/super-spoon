5fc38948fa797888c69f0716eea3ddf4b18d908c
Ayanoo <gagan270903@gmail.com>
2024-12-02 15:35:00 +0530

    test

--- src/app/sos/view/map.jsx
+++ src/app/sos/view/map.jsx
@@ -1,50 +1,58 @@
-import { AdvancedMarker, APIProvider, Map, } from '@vis.gl/react-google-maps';
-import React, { useEffect, useState } from 'react';
+'use client'
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const GoogleMapTest = React.memo(({ location, updateFunction, userInfo }) => {
-    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY; // Replace with your actual Google Maps API key
 
+    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
     const [mapLoaded, setMapLoaded] = useState(false);
-    const [markerPosition, setMarkerPosition] = useState(location.coordinates);
-    if (!location) {
-        return <>No data provided</>;
-    }
+    const [markerPosition, setMarkerPosition] = useState(location?.coordinates);
 
+    // Memoize the map load handler
+    const onLoad = useCallback(() => {
+        setMapLoaded(true);
+    }, []);
 
-    // useEffect(() => {
-    //     // Function to handle updates every 10 seconds
-    //     const intervalId = setInterval(() => {
-    //         updateFunction().then((newLocation) => {
-    //             if (newLocation?.coordinates) {
-    //                 const isEqual = JSON.stringify(location) === JSON.stringify(newLocation);
+    // Update Marker based on old location : 
+    // if old and new location same don't do anything
+    const updateMarkerPosition = useCallback((newLocation) => {
+        if (newLocation?.coordinates) {
+            const isEqual = JSON.stringify(location) === JSON.stringify(newLocation);
+            if (!isEqual) {
+                setMarkerPosition(newLocation.coordinates);
+            }
+        }
+    }, [location]);
 
-    //                 if (!isEqual) {
-    //                     setMarkerPosition(newLocation.coordinates);
-    //                 }
-    //             }
-    //         });
-    //     }, 10000); // Update every 10 seconds
+    // Effect to periodically fetch locations
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            updateFunction()
+                .then(updateMarkerPosition)
+                .catch((error) => console.error("Error fetching location:", error));
+        }, 10000); // Update every 10 seconds
 
-    //     return () => clearInterval(intervalId); // Cleanup on unmount
-    // }, [location, updateFunction]);
+        return () => clearInterval(intervalId);
+    }, [updateFunction, updateMarkerPosition]);
 
     return (
-        <APIProvider apiKey={apiKey} onLoad={() => setMapLoaded(true)} region='IN' >
+        <APIProvider apiKey={apiKey} onLoad={onLoad} region='IN' language='en' >
             <Map
                 mapId='map_id'
-                mapTypeControl={false}
-                mapTypeId='satellite'
-                streetViewControl={false}
-                // renderingType='VECTOR'
-                // reuseMaps={true}
-                center={{ lat: markerPosition.latitude, lng: markerPosition.longitude }}
-                zoom={18}
-                style={{ width: '100%', height: '100%' }}>
-
+                reuseMaps={true}
+                mapTypeId='hybrid'
+                renderingType='VECTOR'
+                style={{ width: '100%', height: '100vh' }}
+                defaultCenter={{ lat: markerPosition.latitude, lng: markerPosition.longitude }}
+                defaultZoom={18}
+                gestureHandling={'greedy'}
+                disableDefaultUI={true}
+            >
                 {mapLoaded && <AdvancedMarker
                     position={{ lat: markerPosition.latitude, lng: markerPosition.longitude }}
-                    title={`${userInfo?.displayName}, Last Update time : ${location.timestamp}`}
+                    title={`${userInfo?.displayName}, Last Update time : ${location?.timestamp}`}
                 />}
+
             </Map>
         </APIProvider>
     );
--- src/app/sos/view/page.jsx
+++ src/app/sos/view/page.jsx
@@ -1,73 +1,72 @@
 "use client";
-import result from "@/app/sos/view/data";
 import MapComponent from "@/app/sos/view/map";
 import axios from "axios";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+const server_url = process.env.SERVER_URL || "https://surakshakawach-mobilebackend-192854867616.asia-south2.run.app";
+
 export default function ViewSOS() {
-    // const params = useSearchParams();
-    // const ticketId = params.get("ticketId");
-    // const firebaseUID = params.get("firebaseUID");
+    const params = useSearchParams();
+    const ticketId = params.get("ticketId") || 'ef8107d9-f38f-4afa-bff6-93152d3018bc';
+    const firebaseUID = params.get("firebaseUID") || 'u7TW7LzsjrZ8QkEPV2Ysa76JDJz1';
     const [getRequestFailed, setGetRequestFailed] = useState(false);
     const [userData, setUserData] = useState(undefined);
     const [ticketData, setTicketData] = useState(undefined);
     const [locationData, setLocationData] = useState([]);
     const [loading, setLoading] = useState(true);
-    const server_url = process.env.SERVER_URL || "https://surakshakawach-mobilebackend-192854867616.asia-south2.run.app";
-    const [activeTab, setActiveTab] = useState(1);
 
 
     async function getTicketDetails() {
-        const res = result;
-        const data = res.data;
-        console.log(data);
-        setUserData(data?.user);
-        setTicketData(data?.ticket);
-        setLocationData(data?.ticket.locationInfo);
-        // try {
-        // const res = await axios.get(server_url + "/api/v1/ticket", { params: { ticketId, firebaseUID } });
+        // const res = result;
 
-        // } catch (e) {
-        //     setGetRequestFailed(true);
-        //     console.error(e);
-        // }
+        try {
+            const { data: res } = await axios.get(server_url + "/api/v1/ticket", { params: { ticketId, firebaseUID } });
+            console.log(res)
+            setUserData(res.data?.user);
+            setTicketData(res.data?.ticket);
+            setLocationData(res.data?.ticket?.locationInfo);
+
+        } catch (e) {
+            setGetRequestFailed(true);
+            console.error(e);
+        }
     }
 
     // Initially fetch ticket data
     useEffect(() => {
         getTicketDetails().then(() => {
             console.log("ticket details fetched");
+            setLoading(false);
         });
-        setLoading(false);
     }, []);
 
     //Again fetch --> when any dependency change
-    // useEffect(() => {
-    //     const intervalId = setInterval(async () => {
-    //         try {
-    //             const res = await axios.get(server_url + "/api/v1/ticket", { params: { ticketId, firebaseUID } });
-    //             const newTicketData = res.data.data.ticket;
+    useEffect(() => {
+        const intervalId = setInterval(async () => {
+            try {
+                const res = await axios.get(server_url + "/api/v1/ticket", { params: { ticketId, firebaseUID } });
+                const newTicketData = res.data.data.ticket;
 
-    //             setTicketData(prevTicketData => {
-    //                 if (!prevTicketData) return newTicketData;
+                setTicketData(prevTicketData => {
+                    if (!prevTicketData) return newTicketData;
 
-    //                 const updatedImages = Array.from(new Set([...prevTicketData.images, ...newTicketData.images]));
+                    const updatedImages = Array.from(new Set([...prevTicketData.images, ...newTicketData.images]));
 
-    //                 return {
-    //                     ...newTicketData,
-    //                     images: updatedImages,
-    //                 };
-    //             });
+                    return {
+                        ...newTicketData,
+                        images: updatedImages,
+                    };
+                });
 
-    //             console.log("Images updated");
-    //         } catch (e) {
-    //             console.error("Error updating ticket details:", e);
-    //         }
-    //     }, 10000);
+            } catch (e) {
+                console.error("Error updating ticket details:", e);
+            }
+        }, 10000);
 
-    //     return () => clearInterval(intervalId);
-    // }, [ticketId, firebaseUID]);
+        return () => clearInterval(intervalId);
+    }, [ticketId, firebaseUID]);
 
     //If any error happen while fetching data
     if (getRequestFailed) {
@@ -92,68 +91,31 @@ export default function ViewSOS() {
         );
     }
 
-    async function fetchLatestLocation() {
+    // Update function to refecth user location after 10sec
+    const fetchLatestLocation = async () => {
         try {
-            // const res = await axios.get(`${server_url}/api/v1/ticket/latest-update`, {
-            //     params: { ticketId, firebaseUID },
-            // });
-            // const data = res.data.data.locationInfo;
-            const res = result;
-            const data = res.data;
-            setLocationData(prev => {
-                prev.push(data);
-                return prev;
+            const { data: resData } = await axios.get(`${server_url}/api/v1/ticket/latest-update`, {
+                params: { ticketId, firebaseUID },
             });
-
+            const data = resData?.data?.locationInfo;
+            // const res = result;
+            // const data = res.data.locationInfo;
+            setLocationData(prev => [...prev, data]);
             return data;
         } catch (e) {
             console.error(e);
-            return undefined;
         }
     }
 
     return (
         <section id="welcome">
 
-            <div className="border-b border-gray-200 dark:border-neutral-700">
-                <nav className="flex gap-x-1" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
-                    <button
-                        type="button"
-                        className="font-bold text-lg py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent whitespace-nowrap hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:hover:text-blue-500"
-                        onClick={() => setActiveTab(1)}
-                    >
-                        Location
-                    </button>
-                    <button
-                        type="button"
-                        className="font-bold text-lg py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent whitespace-nowrap hover:text-blue-600 focus:outline-none focus:text-blue-600"
-                        onClick={() => setActiveTab(2)}
-                    >
-                        Images
-                    </button>
-                    <button
-                        type="button"
-                        className="font-bold text-lg py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent whitespace-nowrap hover:text-blue-600 focus:outline-none focus:text-blue-600"
-                        onClick={() => setActiveTab(3)}
-                    >
-                        Audio
-                    </button>
-                </nav>
-            </div>
-
-            <div className="mt-3">
-                <div hidden={activeTab !== 1}>
-                    <div className="h-[70vh]">
-                        <MapComponent location={locationData?.slice(-1)[0]} userInfo={userData} updateFunction={fetchLatestLocation} />
-                    </div>
-                </div>
-                <div hidden={activeTab !== 2}>
-                    <div className="my-10 py-2">
+            <div className="relative">
+                <div className="h-screen w-screen relative">
+                    <MapComponent location={locationData?.slice(-1)[0]} userInfo={userData} updateFunction={fetchLatestLocation} />
+                    <UserInfo userInfo={userData} />
+                    <div className="absolute left-4 top-5 min-h-screen flex flex-col gap-5" >
                         <ImagesView files={ticketData?.images || []} />
-                    </div>
-                </div>
-                <div hidden={activeTab !== 3}>
-                    <div className="my-10 py-2">
                         <AudioClipsView files={ticketData?.audioClips || []} />
                     </div>
                 </div>
@@ -162,37 +124,80 @@ export default function ViewSOS() {
     );
 }
 
-function ImagesView({ files }) {
+const UserInfo = ({ userInfo }) => {
+    const [pfpImg, setpfpImg] = useState("");
+
+    async function getProfileImage() {
+        try {
+            const { data: res } = await axios.get(server_url + "/api/v1/user/profile-img", { params: { firebaseUID: userInfo?.firebaseUID } });
+            setpfpImg(res?.data?.profile_url)
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // Initially fetch ticket data
+    useEffect(() => {
+        getProfileImage()
+    }, []);
+    return (
+        <div className="absolute right-5 top-6 flex items-center" >
+            <div className="pr-2 pl-4 py-2 bg-[#242424] text-white rounded-l-full " >
+                <h3 className="font-semibold" > {userInfo.displayName} </h3>
+            </div>
+            <div>
+                <Image src={pfpImg} alt="profile" width={50} height={50} className="rounded-full" />
+            </div>
+        </div>
+    )
+}
+
+const ImagesView = ({ files }) => {
+
     if (!files.length) {
         return <>No Images yet!</>;
     }
 
     return (
-        <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-            {files.map(file => (
-                <li key={file} className="col-span-1">
-                    <Image height={1000} width={1000} src={file} alt="IMAGE NOT LOADED" className="object-cover pointer-events-none group-hover:opacity-75 aspect-video" />
-                </li>
-            ))}
-        </ul>
+        <div className="p-6 pt-4 flex flex-col rounded-xl bg-white " >
+            <div>
+                <h3 className="font-semibold text-xl mb-4" > Emergency Image </h3>
+            </div>
+
+            <div className="pb-3 border-b-2 border-b-black mb-3" >
+                <Image src={files[0]} width={325} height={100} alt="IMAGE NOT LOADED" className="object-cover pointer-events-none group-hover:opacity-75 rounded-xl aspect-video" />
+            </div>
+            <ul className={`grid gap-5 ${files.length === 1 ? "grid-cols-1" : "grid-cols-2"}`}>
+                {files.map(file => (
+                    <li key={file} className="rounded-xl relative ">
+                        <Image src={file} width={125} height={125} alt="IMAGE NOT LOADED" className="object-cover pointer-events-none group-hover:opacity-75 rounded-xl aspect-square" />
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
-function AudioClipsView({ files }) {
+const AudioClipsView = ({ files }) => {
     if (!files.length) {
         return <>No Clips!</>;
     }
 
     return (
-        <ul className="grid grid-cols-1 gap-x-4 gap-y-4 xl:gap-x-8">
-            {files.map((file, i) => (
-                <li key={file} className="flex col-span-1">
-                    <p>Recording {i + 1}</p>
-                    <audio controls className="pl-10 object-cover group-hover:opacity-75 aspect-square">
-                        <source src={file} type="audio/mp3" />
-                    </audio>
-                </li>
-            ))}
-        </ul>
+        <div className="p-6 pt-4 flex flex-col rounded-xl bg-white gap-4" >
+            <div>
+                <h3 className="font-semibold text-xl text-center" > Audio </h3>
+            </div>
+
+            <div className="flex flex-col gap-4" >
+                {files.map((file, i) => (
+                    <li key={i}>
+                        <audio controls={true} className="">
+                            <source src={file} type="audio/mp3" />
+                        </audio>
+                    </li>
+                ))}
+            </div>
+        </div>
     );
 }
